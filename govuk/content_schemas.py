4f14824c2689c0e24c6a75ec28ef1dae9af14bbf
Michael Walker <mike@barrucadu.co.uk>
2019-03-02 18:07:18 +0000

    Only have type-specific details hash parsers
    GOV.UK content items are JSON objects with a bunch of fields, one of
    which is 'details'.  I think it's only the format of the 'details'
    that varies depending on type: the other fields all belong to a common
    schema.

--- govuk/content_schemas.py
+++ govuk/content_schemas.py
@@ -37,39 +37,42 @@ def parse_raw(raw):
         raise NoDocumentType()
 
     document_type = raw['document_type']
-    document_type_parser = f'parse_type_{document_type}'
+    document_type_parser = f'parse_details_{document_type}'
 
     if document_type_parser not in globals():
         raise UnknownDocumentType(document_type)
 
     try:
-        return globals()[document_type_parser](raw)
+        body = globals()[document_type_parser](raw['details'])
+        return ContentItem(
+            title=raw['title'],
+            description=raw.get('description', ''),
+            updated_at=raw['public_updated_at'],
+            body=body,
+        )
     except Exception as e:
         raise MalformedContentItem(e)
 
 
-def parse_type_transaction(raw):
-    """Parse a transaction content item."""
-
-    details = raw['details']
+def parse_details_transaction(details):
+    """Parse a transaction content item details hash."""
 
     body = []
+
     if 'introductory_paragraph' in details:
         body.append(markup.text(details['introductory_paragraph']))
+
     body.append(
         markup.web_link(
             details['start_button_text'],
             details['transaction_start_link']))
+
     if 'more_information' in details:
         body.append(markup.heading('More information'))
         body.append(markup.text(details['more_information']))
+
     if 'other_ways_to_apply' in details:
         body.append(markup.heading('Other ways to apply'))
         body.append(markup.text(details['other_ways_to_apply']))
 
-    return ContentItem(
-        title=raw['title'],
-        description=raw.get('description', ''),
-        updated_at=raw['public_updated_at'],
-        body=body,
-    )
+    return body
