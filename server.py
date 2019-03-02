f289922afb63b76d927027bcde5ec3b09e9f0c6a
Michael Walker <mike@barrucadu.co.uk>
2019-03-02 18:20:46 +0000

    Include server IP in Gopher linux
    This is required.

--- server.py
+++ server.py
@@ -11,36 +11,34 @@ import traceback
 BASE_PATH_PATTERN = re.compile('^(/[a-zA-Z0-9\-]+)+/?$')
 
 
-def handler(port):
-    async def inner_handler(reader, writer):
-        raw = await reader.read(4096)
-        request = raw.decode().strip()
-        addr = writer.get_extra_info('peername')
-        print(f'{addr}: "{request}"')
+async def handler(reader, writer):
+    raw = await reader.read(4096)
+    request = raw.decode().strip()
+    ip, port = writer.get_extra_info('sockname')
+    addr = writer.get_extra_info('peername')
+    print(f'{addr}: "{request}"')
 
-        if request in ['', '/']:
-            response = gopher.usage_message(port)
-        elif BASE_PATH_PATTERN.match(request):
-            try:
-                content_item = fetch_content_item(request)
-                response = gopher.render(content_item)
-            except schemas.UnknownDocumentType as e:
-                response = gopher.bad_content_message(
-                    request, f'This page is of type "{e.args[0]}", which is not supported.')
-            except Exception as e:
-                print(f'Exception: {str(e)}')
-                traceback.print_exc(file=sys.stdout)
-                response = gopher.bad_content_message(
-                    request, 'Something went wrong parsing the response from GOV.UK.')
-        else:
-            response = gopher.bad_request_message(request)
+    if request in ['', '/']:
+        response = gopher.usage_message(ip, port)
+    elif BASE_PATH_PATTERN.match(request):
+        try:
+            content_item = fetch_content_item(request)
+            response = gopher.render(content_item)
+        except schemas.UnknownDocumentType as e:
+            response = gopher.bad_content_message(
+                request, f'This page is of type "{e.args[0]}", which is not supported.')
+        except Exception as e:
+            print(f'Exception: {str(e)}')
+            traceback.print_exc(file=sys.stdout)
+            response = gopher.bad_content_message(
+                request, 'Something went wrong parsing the response from GOV.UK.')
+    else:
+        response = gopher.bad_request_message(request)
 
-        writer.write(response.encode())
-        await writer.drain()
+    writer.write(response.encode())
+    await writer.drain()
 
-        writer.close()
-
-    return inner_handler
+    writer.close()
 
 
 def run(ip='127.0.0.1', port=70):
@@ -48,7 +46,7 @@ def run(ip='127.0.0.1', port=70):
     """
 
     loop = asyncio.get_event_loop()
-    coro = asyncio.start_server(handler(port), ip, port, loop=loop)
+    coro = asyncio.start_server(handler, ip, port, loop=loop)
     server = loop.run_until_complete(coro)
 
     print('Gopher server running')
