4b1f3baf731e054c61f161e9d6e8ba558f679c34
Michael Walker <mike@barrucadu.co.uk>
2019-03-02 17:35:27 +0000

    Render markup to gopher

--- gopher.py
+++ gopher.py
@@ -1,3 +1,10 @@
+from markup import Elem
+
+
+class BadMarkup(Exception):
+    pass
+
+
 def usage_message(port):
     """Print a message about how to use the server, with some example
     pages.
@@ -42,5 +49,77 @@ def bad_request_message(request):
     )
 
 
+def wordwrap(string, colwidth=80):
+    """Wrap some text by breaking lines at spaces.
+    """
+
+    if string[0:4] == '  * ':
+        string = string[4:]
+        islist = True
+        colwidth = 76
+    else:
+        islist = False
+
+    out = []
+    line = ''
+    for word in string.split():
+        if line == '':
+            line = word
+            continue
+        if len(line) + len(word) + 1 > colwidth:
+            out.append(line)
+            line = word
+        else:
+            line = f'{line} {word}'
+    if line != '':
+        out.append(line)
+
+    if islist:
+        for i in range(len(out)):
+            if i == 0:
+                out[i] = f'  * {out[i]}'
+            else:
+                out[i] = f'    {out[i]}'
+
+    if out == []:
+        out = ['']
+
+    return out
+
+
 def render(content_item):
-    raise Exception(content_item)
+    """Render a content item as a gopher menu.
+    """
+
+    menu = [
+        f'i{content_item.title}\r\n',
+        f'i{content_item.updated_at}\r\n',
+        f'i\r\n',
+        f'i-------------------------------------------------------------------------------\r\n',
+        f'i\r\n',
+    ]
+    for line in wordwrap(content_item.description):
+        menu.append(f'i{line}\r\n')
+    menu.extend([
+        f'i\r\n',
+        f'i-------------------------------------------------------------------------------\r\n',
+        f'i\r\n',
+    ])
+
+    for item in content_item.body:
+        if item['type'] == Elem.HEADING:
+            menu.append(f'i# {item["text"]}\r\n')
+            menu.append('i\r\n')
+        elif item['type'] == Elem.TEXT:
+            for line in item['text'].split('\n'):
+                for lline in wordwrap(line):
+                    menu.append(f'i{lline}\r\n')
+        elif item['type'] == Elem.WEB_LINK:
+            menu.append('i\r\n')
+            menu.append(
+                f'h{item["text"]} (HTTP link)\tURL:{item["target"]}\t\t\r\n')
+            menu.append('i\r\n')
+        else:
+            raise BadMarkup(item['type'])
+
+    return ''.join(menu)
