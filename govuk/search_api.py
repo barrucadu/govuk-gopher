a0f5d6c7f4ea2b89f1690def668290f35ce95a9f
Michael Walker <mike@barrucadu.co.uk>
2019-03-03 00:54:49 +0000

    Fetch 25 items from search API

--- govuk/search_api.py
+++ govuk/search_api.py
@@ -3,12 +3,13 @@ import requests
 API_PATH = 'https://www.gov.uk/api/search.json'
 
 
-def fetch_raw_search_results(query):
+def fetch_raw_search_results(query, count=25):
     """Query the GOV.UK search API, and don't do any validation or parsing
     beyond interpreting it as JSON.
     """
 
     payload = {f'filter_{field}': value for (field, value) in query.items()}
+    payload['count'] = count
 
     resp = requests.get(f'{API_PATH}', params=payload)
     return resp.json()
